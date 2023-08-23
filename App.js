bc6340fafab59faed310edf7070433eaff57806b
Leonardo Esteves <leonardoestevesoliveira192@hotmail.com>
2023-08-23 03:32:42 -0300

    Add sticker on image

--- App.js
+++ App.js
@@ -8,10 +8,13 @@ import IconButton from "./src/components/IconButton";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import EmojiPicker from "./src/components/EmojiPicker";
+import EmojiList from "./src/components/EmojiList";
+import EmojiSticker from "./src/components/EmojiSticker";
 
 const placeholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [pickedEmoji, setPickedEmoji] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -50,6 +53,9 @@ export default function App() {
           placerholderImageSource={placeholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji !== null ? (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        ) : null}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -76,10 +82,9 @@ export default function App() {
           />
         </View>
       )}
-      <EmojiPicker
-        isVisible={isModalVisible}
-        onClose={onModalClose}
-      ></EmojiPicker>
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
       <StatusBar style="auto" />
     </View>
   );
