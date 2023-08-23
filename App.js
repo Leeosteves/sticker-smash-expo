31b91582abef217a0e32cb49088b539ba914d06a
Leonardo Esteves <leonardoestevesoliveira192@hotmail.com>
2023-08-23 02:07:19 -0300

    Button Component

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, View } from 'react-native';
-import ImageViewer from './src/components/ImageViewer';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-const placeholderImage = require('./assets/images/background-image.png')
+import ImageViewer from "./src/components/ImageViewer";
+import Button from "./src/components/Button";
+
+const placeholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="inverted" />
       <View style={styles.imageContainer}>
-        <ImageViewer placerholderImageSource={placeholderImage}/>
+        <ImageViewer placerholderImageSource={placeholderImage} />
       </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,12 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
-  imageContainer:{
+  imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,14 +1,13 @@
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image } from "react-native";
 
-export default function ImageViewer({placerholderImageSource}) {
-return(<Image style={styles.image} source={placerholderImageSource}/>)
+export default function ImageViewer({ placerholderImageSource }) {
+  return <Image style={styles.image} source={placerholderImageSource} />;
 }
 
 const styles = StyleSheet.create({
-    image:{
-      width: 320,
-      height: 440,
-      borderRadius: 18
-    },
-  });
-  
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
